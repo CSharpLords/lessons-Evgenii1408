24ece6a8a6b31b48924a3847fa3b49c8a0a5bcd2
Evgenii1408 <32613778+Evgenii1408@users.noreply.github.com>
2017-12-03 16:11:53 +0300

    11

---
+++ 09. while/CheckPassword/CheckPassword/ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+          
+        {
+            
+            for (int n = 1;n <= 9;n = n + 1)
+            {
+                Console.WriteLine(n+"x7=" + n*7);
+                
+                Thread.Sleep(1000);
+
+            }
+            Console.Read();
+
+        }
+        }
+    }
+
+
