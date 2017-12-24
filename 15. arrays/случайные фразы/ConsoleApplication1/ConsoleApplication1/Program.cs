e053c4d6eeb4cd52a6a09c33e3109e198f019a7f
Evgenii1408 <32613778+Evgenii1408@users.noreply.github.com>
2017-12-24 16:49:11 +0300

    15

---
+++ 15. arrays/случайные фразы/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+        
+            int n = 0;
+            int a = 0;
+            string tup = null;
+            string tul = null;
+            string tum = null;
+            string[] nouns = { "Вася, Машина, Земля, Утка, Диван, Компьютер" };
+            string[] adverbs = { "быстро, счастливо, томно, редко, шумно, остро" };
+            string[] verbs = { "платит, танцует, бегает, отдыхает, смотрит, болит, ест, ездит" };
+            Random rand = new Random();
+            while (n < 5)
+            {
+                a = rand.Next(0, 4);
+                tup = nouns[a];
+                a = rand.Next(0, 4);
+                tul = adverbs[a];
+                a = rand.Next(0, 4);
+                tum = verbs[a];
+                Console.WriteLine("Ваше слово:" + tup + " " + tul + " " + tum);
+                n = n + 1;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
